54115f8290d81bfdff54b0a50a605eee1d6d83fd
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-17 12:39:51 +0100

    Added inserts file

---
+++ inserts.java
@@ -0,0 +1,25 @@
+// Subscribers ############################################################################
+
+INSERT INTO Subscriber (CreditCard, LastName, FirstName, Birthdate, Adress) 
+VALUES (00122478, "Mouchet", "Alexandre", TO_DATE('17/03/1993', 'dd/mm/yyyy'), "Paris");
+
+INSERT INTO Subscriber (CreditCard, LastName, FirstName, Birthdate, Adress) 
+VALUES (00482956, "Carre", "Ludovic", TO_DATE('08/07/1995', 'dd/mm/yyyy'), "Marseille");
+
+INSERT INTO Subscriber (CreditCard, LastName, FirstName, Birthdate, Adress) 
+VALUES (00996314, "Lefoulon", "Vincent", TO_DATE('24/04/1994', 'dd/mm/yyyy'), "Paris");
+
+INSERT INTO Subscriber (CreditCard, LastName, FirstName, Birthdate, Adress) 
+VALUES (00685321, "Deloche", "Maxime", TO_DATE('13/11/1996', 'dd/mm/yyyy'), "Lyon");
+
+// VehicleClass ###########################################################################
+// Vehicle ################################################################################
+// Station ################################################################################
+// Location ###############################################################################
+// UserClassIllimitedRate #################################################################
+// UserClassLimitedRate ###################################################################
+// SubscriberClass ########################################################################
+// StationVehicle #########################################################################
+// StationLocation ########################################################################
+// StationClass ###########################################################################
+// Spot ###################################################################################
