e83d959fc328fca7b515b5f3d725a582ad2b7e09
piggygenius <ludo.itrts@gmail.com>
2016-11-17 17:35:26 +0100

    We are getting better and better

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,12 @@
-import model.DataAccess;
+import java.util.ArrayList;
+import model.*;
 
 public class Application {
 	public static void main(String[] args){
 		DataAccess model = new DataAccess();
-		int maxSpots = model.getMaxSpots("ISS","BIKE");
+		int maxSpots = model.getMaxSpots("Austerlitz","Velo");
 		System.out.println("Number of empty spots: "+maxSpots);
+		//ArrayList<LocationBill> location_list = model.getLocationBill(1);
 		model.closeConnection();
 	}
 }
--- src/model/DataAccess.java
+++ src/model/DataAccess.java
@@ -1,6 +1,6 @@
 package model;
 
-
+import java.util.ArrayList;
 import java.sql.*;
 
 public class DataAccess {
@@ -14,6 +14,7 @@ public class DataAccess {
 			String user = "carrel";
 			String password = "carrel";
 			this.connection = DriverManager.getConnection(database,user,password);
+			this.connection.setAutoCommit(false);
 		} catch(SQLException e){
 			e.printStackTrace();
 		} catch(ClassNotFoundException e){
@@ -29,14 +30,40 @@ public class DataAccess {
 		}
 	}
 
+	public ArrayList<LocationBill> getLocationBill(int id_location){
+		try {
+			String query = "SELECT StartDate,MaxDuration,HourlyPrice,Deposit FROM FROM VehicleClass,Vehicle,Location WHERE Location.IdLocation = ? AND VehicleClass.ClassName = Vehicle.ClassName AND Location.IdVehicle = Vehicle.IdVehicle";
+			PreparedStatement statement = this.connection.prepareStatement(query);
+			statement.setInt(1,id_location);
+			ResultSet result_set = statement.executeQuery();
+			ResultSetMetaData rsmd = result_set.getMetaData();
+
+			ArrayList<LocationBill> result_list = new ArrayList<LocationBill>();
+			while(result_set.next()){
+				LocationBill location_bill = new LocationBill();
+				location_bill.setDate(result_set.getDate(1));
+				location_bill.setMaxDuration(result_set.getInt(2));
+				location_bill.setHourlyPrice(result_set.getFloat(3));
+				location_bill.setDeposit(result_set.getFloat(4));
+				result_list.add(location_bill);
+			}
+			result_set.close();
+			statement.close();
+			return result_list;
+		} catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	public int getMaxSpots(String station_name,String class_name){
 		try {
-			PreparedStatement statement = this.connection.prepareStatement("SELECT MaxSeats FROM StationClass WHERE StationName = ? and ClassName = ?");
+			String query = "SELECT MaxSpots FROM StationClass WHERE StationName = ? and ClassName = ?";
+			PreparedStatement statement = this.connection.prepareStatement(query);
 			statement.setString(1,station_name);
 			statement.setString(2,class_name);
-
 			ResultSet result_set = statement.executeQuery();
-			result_set.first();
+			result_set.next();
 			int result = result_set.getInt(1);
 			result_set.close();
 			statement.close();
@@ -46,4 +73,5 @@ public class DataAccess {
 			return -1;
 		}
 	}
+
 }
