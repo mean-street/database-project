c54678e20570aea3404e50ee197c82bd05fb956c
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-18 11:59:54 +0100

    Removed useless test

--- sql/test.sql
+++
@@ -1,5 +0,0 @@
-SELECT Vehicle.ClassName, SUM(StationLocation.EndDate - Location.StartDate)
-FROM Location, StationLocation, Vehicle
-WHERE Location.IdLocation = StationLocation.IdLocation
-AND Vehicle.IdVehicle = Location.IdVehicle
-GROUP BY Vehicle.ClassName;
