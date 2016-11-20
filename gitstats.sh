b5b1ec3cdc7a9104627aadee542d26a16e78ae3c
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-20 23:35:53 +0100

    Removed useless stats file

--- gitstats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "################################# Total amount of changes #################################";echo ""
-
-git log --shortstat --pretty="%cE" | sed 's/\(.*\)@.*/\1/' | grep -v "^$" | awk 'BEGIN { line=""; } !/^ / { if (line=="" || !match(line, $0)) {line = $0 "," line }} /^ / { print line " # " $0; line=""}' | sort | sed -E 's/# //;s/ files? changed,//;s/([0-9]+) ([0-9]+ deletion)/\1 0 insertions\(+\), \2/;s/\(\+\)$/\(\+\), 0 deletions\(-\)/;s/insertions?\(\+\), //;s/ deletions?\(-\)//' | awk 'BEGIN {name=""; files=0; insertions=0; deletions=0;} {if ($1 != name && name != "") { print name ": " files " files changed, " insertions " insertions(+), " deletions " deletions(-), " insertions-deletions " net"; files=0; insertions=0; deletions=0; name=$1; } name=$1; files+=$2; insertions+=$3; deletions+=$4} END {print name ": " files " files changed, " insertions " insertions(+), " deletions " deletions(-), " insertions-deletions " net";}'
-
-echo"";echo "#################################### Current ownership #####################################";echo ""
-
-git ls-tree -r -z --name-only HEAD -- src/ sql/ conception/ | xargs -0 -n1 git blame --line-porcelain HEAD | grep -a "^author " | sort | uniq -c | sort -nr
